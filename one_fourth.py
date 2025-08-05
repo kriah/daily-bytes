c0e6eca1ff8b0d1a9c472e8608977169aa629713
kriah <57598256+kriah@users.noreply.github.com>
2025-08-05 00:06:10 -0400

    Update function

--- one_fourth.py
+++ one_fourth.py
@@ -11,20 +11,21 @@ Ex: Given the following nums…
 nums = [1, 2, 3, 4], return -1.
 '''
 
-nums = [1, 2, 2, 3, 4]
+nums = [1, 2, 3, 4]
+nums1 = [1, 2, 2, 3, 4]
 nums2 = [1, 1, 1, 3, 4, 1, 2, 2]
 
 def oneFourth(nums):
-    uniqueValues = set(nums)
-    length = len(uniqueValues)
-    min = length / 4
-    fourth = 0
+    uniqueValues = set(nums) # set of uniques number from nums
+    min = len(nums) / 4 # value of 1/4 of the array
+    fourth = -1
 
     for num in uniqueValues:
-        if nums.count(num) > min:
+        numCount = nums.count(num)
+        if numCount > min and numCount > nums.count(fourth):
             fourth = num
 
     return fourth
     
 
-print(oneFourth(nums2))
+print(oneFourth(nums))
