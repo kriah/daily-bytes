994a79d266faa7ed522f3be245d0bf2dabb90274
kriah <57598256+kriah@users.noreply.github.com>
2025-08-08 17:07:22 -0400

    Add Anagram Elements

---
+++ anagram_elements.py
@@ -0,0 +1,27 @@
+''' Anagram Elements
+
+You are given two integer arrays, A and B. B is an anagram of A meaning that B contains all the same elements of A but 
+in a different order. Return an array that represents a mapping from every element in A to which index it occurs at in B.
+Note: You may assume every element in A is unique.
+
+Ex: Given the following A and B…
+
+A = [8, 23, 2], B = [2, 23, 8], return [2, 1, 0] (8 appears at index 2 in B, 23 appears at index 1 in B, and 2 appears at index 0 in B).
+Ex: Given the following A and B…
+
+A = [9, 3, 1, 5, 2, 4], B = [2, 5, 1, 3, 4, 9], return [5,3,2,1,0,4].
+'''
+
+A = [9, 3, 1, 5, 2, 4]
+B = [2, 5, 1, 3, 4, 9]
+
+
+def anagram_mapping(A, B):
+    indexes = []
+    for item in A:
+        indexValue = B.index(item)
+        indexes.append(indexValue)
+
+    return indexes
+
+print(anagram_mapping(A, B))
