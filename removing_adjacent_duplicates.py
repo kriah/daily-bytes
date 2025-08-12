1ec503d4d908dd97596459aae04e17281e6e01b7
kriah <57598256+kriah@users.noreply.github.com>
2025-08-12 00:04:20 -0400

    Add Removing Adjacent Duplicates

---
+++ removing_adjacent_duplicates.py
@@ -0,0 +1,22 @@
+''' Removing Adjacent Duplicates
+Given a string s containing only lowercase letters, continuously remove adjacent characters that are the same and return the result.
+
+Ex: Given the following strings...
+
+s = "abccba", return ""
+s = "foobar", return "fbar"
+s = "abccbefggfe", return "a"
+'''
+
+s = "abccba"
+s1 = "foobar"
+s2 = "abccbefggfe"
+
+def remove_duplicates(s):
+    index = 0
+    new_string = ""
+
+    
+    return new_string
+
+print(remove_duplicates(s1))
