049605a9eedc42d6a5f2cc15857e964f415eb8ce
kriah <57598256+kriah@users.noreply.github.com>
2025-08-17 23:03:55 -0400

    Update function

--- length_of_increase.py
+++ length_of_increase.py
@@ -9,22 +9,32 @@ Ex: Given the following nums.
 nums = [3, 4, 1, 2, 8], return 3.
 '''
 
-nums = [1, 2, 3]
-nums1 = [3, 4, 1, 2, 8]
-nums2 = [3, 4, 5, 2, 8]
+nums = [1, 2, 3] #returns 3
+nums1 = [3, 4, 1, 2, 8] # returns 3
+nums2 = [3, 4, 5, 2, 8] # returns 3
+nums3 = [5, 4, 3, 2, 1]
 
 def increasing_interval(nums):
     count = 1
-    current_count = 0
+    current_count = 1
 
+    # iterate through the list up until the last item
     for i in range(len(nums)-1):
+        # checks if the current item is less than the next item
         if (nums[i] < nums[i+1]):
             count += 1
+        
         else:
             if (count > current_count): 
                 current_count = count
             count = 1
+        
+        if (count > current_count): 
+                current_count = count
 
     return current_count
 
+print("Count:", increasing_interval(nums))
+print("Count:", increasing_interval(nums1))
 print("Count:", increasing_interval(nums2))
+print("Count:", increasing_interval(nums3))
