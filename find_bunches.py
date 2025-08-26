ca895ce7ad592bb2b276647680390d8b193a3018
kriah <57598256+kriah@users.noreply.github.com>
2025-08-26 18:28:03 -0400

    Update function

--- find_bunches.py
+++ find_bunches.py
@@ -11,25 +11,33 @@ Ex: Given the following string S...
 S = “aaabbcddddd”, return [[0,2],[6,10]].
 '''
 
-s1 = 'aaabbbccc' # [[0,2], [3,5], [6,8]]
-s2 = 'aaabbcddddd' # [[0,2], [6,10]]
+s1 = 'aaabbbcccdd' # [[0,2], [3,5], [6,8]]
+s2 = 'aaabbcdddddee' # [[0,2], [6,10]]
 
 def find_intervals(s):
+    count = 0 # counter to track number of letters
+    start = 0 # index of the start of interval
     letter = s[0]
-    start = 0
-    intervals = []
-
+    intervals = [] # will contain the intervals
+    
+    # iterate through array to find intervals
     for i in range(len(s)):
-        if i == len(s)-1:
-            intervals.append([start,i])
-        elif s[i] == letter:
-            print(letter)
+        
+        if s[i] == letter:
+            count += 1 # increase count by 1
+            
+            # if item is the last element in array and there's
+            # more than 2 of the same letter then append interval
+            if i == len(s)-1 and count > 2:
+                intervals.append([start,i])
         else:
-            letter = s[i]
-            intervals.append([start,i-1])
-            start = i
+            if count > 2:
+                intervals.append([start,i-1])
+            letter = s[i] # update next letter
+            start = i # update start of next interval
+            count = 1 # reset count
 
     return intervals
 
-
 print(find_intervals(s1))
+print(find_intervals(s2))
