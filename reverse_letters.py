48dc0906929a66a5e9ce186f01d466c9f725bf58
kriah <57598256+kriah@users.noreply.github.com>
2025-09-02 06:31:22 -0400

    Add comments

--- reverse_letters.py
+++ reverse_letters.py
@@ -14,15 +14,18 @@ def reverse_letters(s):
     middle = len(s)//2
 
     while (left_index < middle):
+        # if item isn't in the alphabet then go to next item
         if(not s[left_index].isalpha()):
             left_index += 1
+        # if item isn't in the alphabet then go to next item
         elif(not s[right_index].isalpha()):
             right_index -= 1
         else: 
+            # switch the values at the left and right index
             new_string = s[:left_index] + s[right_index] + s[left_index+1: right_index] + s[left_index] + s[right_index+1:]
             s = new_string
-            left_index += 1
-            right_index -= 1
+            left_index += 1 # go to the next item from the left
+            right_index -= 1 # go to the next item from the right
 
     return s
 
