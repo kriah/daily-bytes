1ba5087df90b8b3816aa0ca16a1382a4326dcbc6
kriah <57598256+kriah@users.noreply.github.com>
2025-09-06 21:45:38 -0400

    Add comments

--- reverse_vowels.py
+++ reverse_vowels.py
@@ -15,15 +15,19 @@ def reverse_vowels(s):
     right_index = len(s)-1
 
     while(left_index < right_index):
+        # if item from the left is not a vowel then move right to next item
         if(not s[left_index] in vowels):
             left_index += 1
+        # if item from the right is not a vowel the move left to the next time
         elif(not s[right_index] in vowels):
             right_index -= 1
+        # when both items are vowels
         else:
+            # switch the value at the left index with the value at the right index
             new_string = s[:left_index] + s[right_index] + s[left_index+1: right_index] + s[left_index] + s[right_index+1:]
             s = new_string
-            left_index += 1
-            right_index -= 1
+            left_index += 1 # move to next item on the right
+            right_index -= 1 # move the next item on the left
 
     return s
 
