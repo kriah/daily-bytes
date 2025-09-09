061e88d01ef3062bc6438727036c3b48f6ccafec
kriah <57598256+kriah@users.noreply.github.com>
2025-09-09 10:28:44 -0400

    Add Merge String problem

--- merge_string.py
+++ merge_string.py
@@ -7,5 +7,18 @@ s = "abc", t = "def", return "adbecf".
 '''
 
 s = "abc"
-t = "defgh" # return "adbecfgh"
+t = "defgh"
 
+def merge_string(s, t):
+    new_string = ""
+    while s != "" and t != "":
+        # add first item from s and t to the new string
+        new_string += s[0] + t[0]
+        s = s[1:] # remove first item from string
+        t = t[1:] # remove first item from string
+    
+    # add remaining string to new string
+    new_string += s + t
+    return new_string
+
+print(merge_string(s, t))
